21be24af2cf3021e1a709c7a373bc61e4d386f24
Amber Thomas <ProQuestionAsker@gmail.com>
2021-03-26 13:50:03 -0700

    fixing ability to copy code

--- src/components/templates-code.js
+++ src/components/templates-code.js
@@ -5,12 +5,16 @@ import InView from '!!raw-loader!./../../templates/InView.svelte';
 import Number from '!!raw-loader!./../../templates/Number.svelte';
 import ProgressDots from '!!raw-loader!./../../templates/ProgressDots.svelte';
 import Scatterplot from '!!raw-loader!./../../templates/Scatterplot.svelte';
-import Toggle from '!!raw-loader!./../../templates/Toggle.svelte';
+import ButtonSet from '!!raw-loader!./../../templates/ButtonSet.svelte';
 import Checkbox from '!!raw-loader!./../../templates/Checkbox.svelte';
 import Radio from '!!raw-loader!./../../templates/Radio.svelte';
 import Switch from '!!raw-loader!./../../templates/Switch.svelte';
 import CollapsibleSection from '!!raw-loader!./../../templates/CollapsibleSection.svelte';
 import Autocomplete from '!!raw-loader!./../../templates/Autocomplete.svelte';
+import Pagination from '!!raw-loader!./../../templates/Pagination.svelte';
+import Table from '!!raw-loader!./../../templates/Table.svelte';
+import CodeBlock from '!!raw-loader!./../../templates/CodeBlock.svelte';
+import DataDownload from '!!raw-loader!./../../templates/DataDownload.svelte';
 
 export default {
 	BriefMessage,
@@ -20,10 +24,14 @@ export default {
 	Number,
 	ProgressDots,
 	Scatterplot,
-	Toggle,
+	ButtonSet,
 	Checkbox,
 	Radio,
 	Switch,
 	CollapsibleSection,
-	Autocomplete
+	Autocomplete,
+	Pagination, 
+	Table,
+	CodeBlock,
+	DataDownload
 };
